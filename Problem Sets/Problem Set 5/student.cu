7d1d64c6ee55e424d8cbef630e57a49e9d6360fc
andabi <andabi412@gmail.com>
2019-05-25 21:46:48 +0900

    prob5: edit makefile, add serial version code

--- Problem Sets/Problem Set 5/student.cu
+++ Problem Sets/Problem Set 5/student.cu
@@ -1,54 +1,52 @@
 /* Udacity HW5
-   Histogramming for Speed
+ Histogramming for Speed
 
-   The goal of this assignment is compute a histogram
-   as fast as possible.  We have simplified the problem as much as
-   possible to allow you to focus solely on the histogramming algorithm.
+ The goal of this assignment is compute a histogram
+ as fast as possible.  We have simplified the problem as much as
+ possible to allow you to focus solely on the histogramming algorithm.
 
-   The input values that you need to histogram are already the exact
-   bins that need to be updated.  This is unlike in HW3 where you needed
-   to compute the range of the data and then do:
-   bin = (val - valMin) / valRange to determine the bin.
+ The input values that you need to histogram are already the exact
+ bins that need to be updated.  This is unlike in HW3 where you needed
+ to compute the range of the data and then do:
+ bin = (val - valMin) / valRange to determine the bin.
 
-   Here the bin is just:
-   bin = val
+ Here the bin is just:
+ bin = val
 
-   so the serial histogram calculation looks like:
-   for (i = 0; i < numElems; ++i)
-     histo[val[i]]++;
+ so the serial histogram calculation looks like:
+ for (i = 0; i < numElems; ++i)
+ histo[val[i]]++;
 
-   That's it!  Your job is to make it run as fast as possible!
+ That's it!  Your job is to make it run as fast as possible!
 
-   The values are normally distributed - you may take
-   advantage of this fact in your implementation.
+ The values are normally distributed - you may take
+ advantage of this fact in your implementation.
 
-*/
-
+ */
 
 #include "utils.h"
 
 __global__
-void yourHisto(const unsigned int* const vals, //INPUT
-               unsigned int* const histo,      //OUPUT
-               int numVals)
+void
+histoSerial (const unsigned int* const vals, unsigned int* const histo,
+	     int numVals)
 {
-  //TODO fill in this kernel to calculate the histogram
-  //as quickly as possible
-
-  //Although we provide only one kernel skeleton,
-  //feel free to use more if it will help you
-  //write faster code
+  for (int i = 0; i < numVals; i++)
+    {
+      histo[vals[i]] += 1;
+    }
 }
 
-void computeHistogram(const unsigned int* const d_vals, //INPUT
-                      unsigned int* const d_histo,      //OUTPUT
-                      const unsigned int numBins,
-                      const unsigned int numElems)
+// TODO parallel code
+// TODO considering data dist (normal dist)
+
+void
+computeHistogram (const unsigned int* const d_vals, //INPUT
+    unsigned int* const d_histo,      //OUTPUT
+    const unsigned int numBins, const unsigned int numElems)
 {
-  //TODO Launch the yourHisto kernel
+  histoSerial <<<1, 1>>> (d_vals, d_histo, numElems);
 
-  //if you want to use/launch more than one kernel,
-  //feel free
-
-  cudaDeviceSynchronize(); checkCudaErrors(cudaGetLastError());
+  cudaDeviceSynchronize ();
+  checkCudaErrors(cudaGetLastError ());
 }
